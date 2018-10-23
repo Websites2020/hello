9b7b4fb9d7e7860599062b1a46374934b552ae69
SOIP <38543876+sumofitsparts@users.noreply.github.com>
2018-10-23 12:40:11 -0400

    first

---
+++ hello.cs
@@ -0,0 +1,10 @@
+// My very first C# program
+
+public class Hello
+{
+	// The program begins inside Main
+	public static void Main()
+	{
+		System.Console.Out.WriteLine ("Hello, World!");
+	}
+} // end of Hello
